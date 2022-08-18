2e5df2511bd16b1d4ab37277e70938d4b560ff61
Asadul islam <islamasadul327@gmail.com>
2022-08-18 12:31:40 +0600

    update release

--- src/constant/index.tsx
+++ src/constant/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const Api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_HOST_URL,
+  baseURL: 'https://backend-bookings.herokuapp.com/api',
 })
 
 Api.interceptors.request.use(async (config: any) => {
